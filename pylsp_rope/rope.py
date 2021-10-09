77df579a20264ceaf2d75b5d8abc0844a5ec7d18
Lie Ryan <lie.1296@gmail.com>
2021-10-10 07:54:25 +1100

    Forgot to add rope.py

---
+++ pylsp_rope/rope.py
@@ -0,0 +1,11 @@
+from rope.base.change import ChangeSet, Change
+from rope.base.project import Project
+from rope.base.resources import Resource
+
+
+__all__ = [
+    'Change',
+    'ChangeSet',
+    'Project',
+    'Resource',
+]
